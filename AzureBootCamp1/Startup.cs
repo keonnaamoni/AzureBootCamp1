282820ab7aa89a947338ff40b7d505a3d40a1c20
keonnaamoni <keonnaevans@gmail.com>
2015-04-25 12:53:20 -0400

    AzureBootCamp Tutorial

---
+++ AzureBootCamp1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzureBootCamp1.Startup))]
+namespace AzureBootCamp1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
